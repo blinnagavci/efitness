f818199b6f96ae400704513ff676558a2df4749a
Labian Gashi <labiangashi@gmail.com>
2016-11-25 16:55:59 +0100

    Fillimi i programimit te rubrikave per "members" dhe stilizimi i tyre

--- front/index.php
+++ front/index.php
@@ -39,6 +39,39 @@
     </div>
 </div>
 <div class="main-right">
-
+    <h1>Manage Member</h1>
+    <div class="main-box">
+        <form id="member-form">
+            <div class="left-form-content">
+                <label>First Name</label>
+                <input type="text" id="member_firstname" placeholder="First Name"/> 
+                <label>Last Name</label>
+                <input type="text" id="member_surname" placeholder="Last Name"/> 
+                <label>Residential Address</label>
+                <input type="text" id="member_name" placeholder="Enter Residential Address"/> 
+                <label>City</label>
+                <input type="text" id="member_city" placeholder="Enter City"/> 
+                <label>Telephone No.</label>
+                <span class="telephone">
+                    <input type="text" id="member_telephone" placeholder="Enter Telephone Number"/>
+                </span>
+                <label>Mobile No.</label>
+                <span class="telephone">
+                    <input type="text" id="member_mobile" placeholder="Enter Mobile Number"/>
+                </span>
+                <label>E-mail</label>
+                <span class="email">
+                    <input type="email" id="member_email" placeholder="Enter Email"/>
+                </span>
+            </div>
+            <div class="right-form-content">
+                <label>Member Photo</label>
+                <img class="member-photo" src="repository/no_image.png" alt="Photo"/>
+                <label>Upload from Computer</label>
+                <input type="file" id="member_upload"/>
+            </div>
+        </form>
+    </div>
 </div>
-<?php require('parts/footer.php'); ?>
+<?php
+require('parts/footer.php');
