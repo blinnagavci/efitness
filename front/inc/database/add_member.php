f155f8b4af4a008154396f0e20758d33138a26e9
DESKTOP-BOO0Q8H\Blini <blinnagavci@gmail.com>
2016-12-01 20:02:03 +0100

    Shtimi i fajllave db_connect dhe add_member.php si dhe ndryshimi i imazhit

---
+++ front/inc/database/add_member.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST['add'])) {
+    $firstname = $_POST['member_firstname'];
+//    $lastname = addslashes($_POST['lastname']);
+//    $email = addslashes($_POST['email']);
+//    $phoneno = addslashes($_POST['phoneno']);
+//    $gender = addslashes($_POST['gender']);
+//    $tabs = implode(', ', $_POST['tabs']);
+//    $referrer = addslashes($_POST['referrer']);
+//    $comment = addslashes($_POST['comment']);
+
+
+    $sql = "INSERT INTO member (firstname) 
+	  			VALUES ('$firstname')";
+
+    $retval = mysqli_query($conn, $sql);
+    if (!$retval) {
+        die('Could not enter data...' . mysqli_connect_error());
+    } else {
+        echo "<script type='text/javascript'> window.alert('Member added successfully!.'); </script>";
+    }
+    mysqli_close($conn);
+    header("refresh:0; url=../index.php");
+}
+?>
--- front/parts/header.php
+++ front/parts/header.php
@@ -23,4 +23,5 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
     </head>
     <body>
+        <?php include('inc/database/db_connect.php'); ?>
         <div class="se-pre-con"></div>
